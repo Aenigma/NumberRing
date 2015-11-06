72999b091a10f0d9661da0866c257f176c941a71
Kevin Raoofi <nullindev@gmail.com>
2015-11-06 10:42:09 -0500

    Instructions are now parsed into Operation objects.

---
+++ src/main/java/edu/frostburg/computerclub/edu/frostburg/cosc489/numberring/service/Instructions.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2015 Kevin Raoofi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.frostburg.computerclub.edu.frostburg.cosc489.numberring.service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author Kevin Raoofi
+ */
+public class Instructions {
+
+    List<Integer> initializeData;
+
+    List<Operation> operations;
+
+    private Instructions(List<Integer> initializeData,
+            List<Operation> operations) {
+        this.initializeData = initializeData;
+        this.operations = operations;
+    }
+
+    private static final Pattern INIT_PATTERN = Pattern.compile(
+            "^(?:\\s*\\d+\\s*)+$");
+
+    private static final Pattern OPERATION_PATTERN = Pattern.compile(
+            "^\\w+\\s*(?:\\d+)?$");
+
+    public static Instructions readFile(Path p) throws IOException {
+        List<String> lines = Files.readAllLines(p);
+
+        List<Integer> initialData = Arrays.stream(lines.stream()
+                .filter(INIT_PATTERN.asPredicate())
+                .findAny()
+                .map(s -> s.split("\\s"))
+                .get())
+                .map(s -> Integer.parseInt(s))
+                .collect(Collectors.toList());
+
+        List<Operation> operations = lines.stream()
+                .filter(OPERATION_PATTERN.asPredicate())
+                .map(Operation::readOperation)
+                .collect(Collectors.toList());
+
+        return new Instructions(initialData, operations);
+    }
+
+    @Override
+    public String toString() {
+        return "Instructions{" + "initializeData=" + initializeData
+                + ", operations=" + operations + '}';
+    }
+
+    public static void main(String... args) throws IOException {
+        Path input
+                = Paths.get("../")
+                .toAbsolutePath()
+                .normalize()
+                .resolve("COSC489_PT_2158_Input.txt");
+
+        Instructions readFile = Instructions.readFile(input);
+        System.out.println(readFile);
+    }
+}
---
+++ src/main/java/edu/frostburg/computerclub/edu/frostburg/cosc489/numberring/service/Operation.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2015 Kevin Raoofi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.frostburg.computerclub.edu.frostburg.cosc489.numberring.service;
+
+/**
+ *
+ * @author Kevin Raoofi
+ */
+class Operation {
+
+    private final OperationType operation;
+    private final int operand;
+
+    private Operation(OperationType operation, int operand) {
+        this.operation = operation;
+        this.operand = operand;
+    }
+
+    public static Operation readOperation(String line) {
+        final String[] tokens = line.split("\\s");
+        final OperationType operation;
+        int operand = 0;
+
+        operation = OperationType.valueOf(tokens[0].toUpperCase());
+
+        if (operation.isConsumer()) {
+            operand = Integer.parseInt(tokens[1]);
+        }
+
+        return new Operation(operation, operand);
+    }
+
+    @Override
+    public String toString() {
+        return "Operation{" + "operation=" + operation + ", operand=" + operand
+                + '}';
+    }
+
+    enum OperationType {
+
+        APPEND(true),
+        DELETE(false),
+        INSERT(true),
+        JUMP(true),
+        MOVE(true),
+        SORT(false),
+        PRINT(false);
+
+        private final boolean consumer;
+
+        private OperationType(boolean consumer) {
+            this.consumer = consumer;
+        }
+
+        boolean isConsumer() {
+            return consumer;
+        }
+
+    }
+}
